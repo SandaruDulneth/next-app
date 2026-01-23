37429ac95494059610915e45157b1696f72a78f8
SandaruDulneth <sdulneth29@gmail.com>
2026-01-23 23:12:11 +0530

    Neon/Stack auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { NavBar } from "@/components/nav/nav-bar";
 import { WikiCard } from "@/components/ui/wiki-card";
 
 export default function Home() {
   return (
     <div>
-     <NavBar/>
       <main className="max-w-2xl mx-auto mt-10 flex flex-col gap-6">
         <WikiCard
           title="Complete Intro to React"
